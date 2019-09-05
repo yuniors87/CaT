f9796623b011a703572b502d7263587ec0f444f5
yuniors87 <yuniors87@gmail.com>
2019-09-04 22:18:58 -0400

    intentando cambiar estado

--- src/components/list.js
+++ src/components/list.js
@@ -16,22 +16,35 @@ const Item = styled.div`
   margin: 7px;
   padding: 0 15px;
   box-shadow: 0 4px 5px rgba(0, 0, 0, 0.2);
+  background: ${props => (props.activo ? "lightgreen" : "red")};
 `
 
 const LabelItem = styled.p`
   margin: 0;
 `
 
-const List = ({ tareas }) => (
-  <StyledList>
-    {tareas.map(tarea => (
-      <Item key={tarea.valor} data-valor={tarea.valor}>
-        <LabelItem>
-          {tarea.nombre} ({tarea.valor})
-        </LabelItem>
-      </Item>
-    ))}
-  </StyledList>
-)
-
+function List({ tareas, onClick, cambiar }) {
+  function handleChange(event) {
+    // Here, we invoke the callback with the new value
+    // props.onChange(event.target.value);
+    console.log("click desde la lista")
+    onClick(event.target.value)
+    console.log(event.target.getAttribute("data-valor"))
+  }
+  return (
+    <StyledList>
+      {tareas.map(tarea => (
+        <Item
+          key={tarea.valor}
+          data-valor={tarea.valor}
+          activo={tarea.activo}
+          onClick={cambiar}
+        >
+          {/* <LabelItem> */}
+          {tarea.nombre} ({tarea.valor}){/* </LabelItem> */}
+        </Item>
+      ))}
+    </StyledList>
+  )
+}
 export default List
--- src/pages/index.js
+++ src/pages/index.js
@@ -29,19 +29,34 @@ function IndexPage() {
     tareasBase.map(tarea => ({
       nombre: tarea.nombre,
       valor: tarea.valor,
-      seleccionado: false
+      activo: false,
     }))
   )
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    console.log("clicl desde index")
+  }
+
+  const cambiar = e => {
+    // console.log(e.target.getAttribute("data-valor"))
+    const val = e.target.getAttribute("data-valor")
+    const nuevoEstado = items
+    const objIndex = nuevoEstado.findIndex(obj => obj.valor == val)
+    console.log(items[objIndex])
+    nuevoEstado[objIndex].activo = !nuevoEstado[objIndex].activo
+    setItems(nuevoEstado)
+    console.log(objIndex)
+    console.log(items[objIndex])
+
+  }
 
   return (
     <Layout>
       <h1>Pruebas</h1>
-      <List tareas={items} />
+      <List tareas={items} onClick={handleSubmit} cambiar={cambiar} />
       <br />
       <Link to="/page-2">Pagina 2</Link>
     </Layout>
   )
 }
 export default IndexPage
-
-// TODO: cambiar el estado cuando se de click en la tarea
